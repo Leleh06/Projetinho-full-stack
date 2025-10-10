92108e3507f3cd5f45b93d75241945b86ce614da
Leleh06 <leticyasilvad1608@gmail.com>
2025-10-09 22:06:09 -0300

    Feat: Adicionando Logs

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import User from './components/User/user';
 import Menu from './components/Menu/Menu';
+import Login from './components/Login/Login';
 
 function App() {
 
   return (
     <>
-      <h1>Heloo, its me</h1>
+     {/* <h1>Heloo, its me</h1>  */}
       {/* <User/> */}
-      <Menu/>
+      {/* <Menu/> */}
+      <Login/>
     </>
   )
 }
